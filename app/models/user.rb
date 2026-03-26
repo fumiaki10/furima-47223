1c466eb546dca4903c1e012d3e1e770ba909db1e
fumiaki10 <72801321+fumiaki10@users.noreply.github.com>
2026-03-26 18:46:36 +0900

    バリデーションの追加実装
    姓と名を登録する際の条件を追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,16 @@ class User < ApplicationRecord
     with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   }, if: :password_present?
 
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
   private
 
   def password_present?
