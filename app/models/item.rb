360faa5aa2f6fbe2d5f9314f88e5df76555cdfb5
fumiaki10 <72801321+fumiaki10@users.noreply.github.com>
2026-03-28 17:53:38 +0900

    ActiveHash導入と選択項目実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
 
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
   has_one_attached :image
 end
