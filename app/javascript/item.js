1971ea53abded3697c21efdd90740e8674fbd8f7
fumiaki10 <72801321+fumiaki10@users.noreply.github.com>
2026-03-29 18:06:31 +0900

    レビュー依頼修正
    ・出品機能では実装しないコードの修正
    ・出品失敗後、出品機能が正しく動くよう修正

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,22 +1,27 @@
+const price = () => {
 
-document.addEventListener("turbo:load", () => {
+
+
   const priceInput = document.getElementById("item-price");
 
   if (!priceInput) return;
 
   priceInput.addEventListener("input", () => {
-    const price = parseInt(priceInput.value, 10);
+    const priceValue = parseInt(priceInput.value, 10);
 
-    if (isNaN(price)) {
-      document.getElementById("add-tax-price").textContent = "0";
-      document.getElementById("profit").textContent = "0";
+    if (isNaN(priceValue)) {
+      document.getElementById("add-tax-price").textContent = "";
+      document.getElementById("profit").textContent = "";
       return;
     }
 
-    const tax = Math.floor(price * 0.1);
-    const profit = price - tax;
+    const tax = Math.floor(priceValue * 0.1);
+    const profit = priceValue - tax;
 
     document.getElementById("add-tax-price").textContent = tax;
     document.getElementById("profit").textContent = profit;
   });
-});
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
