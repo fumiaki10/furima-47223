72a9c7a81196e00cd60bed8d7b5bb89bbde3f1e7
fumiaki10 <72801321+fumiaki10@users.noreply.github.com>
2026-03-29 11:37:37 +0900

    重複した価格のエラー表示を修正
    ・空欄時に数値エラーと空欄エラーが重複して出ていたので、空欄のエラーのみ出るよう修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -31,5 +31,6 @@ class Item < ApplicationRecord
               only_integer: true,
               greater_than_or_equal_to: 300,
               less_than_or_equal_to: 9_999_999
-            }
+            },
+            unless: -> { price.blank? }
 end
