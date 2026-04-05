0cda6caf0c20d3b9daae0b6019a24354fb4dc6f8
fumiaki10 <72801321+fumiaki10@users.noreply.github.com>
2026-04-05 10:39:19 +0900

    単体テストの土台準備

---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,11 @@
+require 'rails-helper'
+
+Rspec.describe OrderAddress, type: :model do
+  before do
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
+  end
+  describe 'ユーザー購入機能' do
+  end
+end
